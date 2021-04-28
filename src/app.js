3cd05b5d09602ee32b6e20d38483f01bec9e2e5c
Vlad Suschevskiy <rts123adc123@gmail.com>
2021-04-28 23:14:33 +0300

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,1 @@
-jQuery(document).ready(function(){
-  $(".button a").click(function(){
-      $(".overlay").fadeToggle(200);
-     $(this).toggleClass('btn-open').toggleClass('btn-close');
-  });
-});
-$('.overlay').on('click', function(){
-  $(".overlay").fadeToggle(200);   
-  $(".button a").toggleClass('btn-open').toggleClass('btn-close');
-  open = false;
-});
+
