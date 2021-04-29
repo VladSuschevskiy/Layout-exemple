07aa8e87e53c936543e9316f6ec64d01525ac924
Vlad Suschevskiy <rts123adc123@gmail.com>
2021-04-29 18:35:47 +0300

    meadia

--- src/js/style.modules.js
+++ src/js/style.modules.js
@@ -1,8 +1,10 @@
 import '../styles/main.scss';
+import '../styles/overall.scss';
 import '../styles/header.scss';
 import '../styles/filter.scss';
 import '../styles/footer.scss';
 import '../styles/normalize.scss';
+import '../styles/card.scss'
 import '../app.js';
 import '../template.html';
 
